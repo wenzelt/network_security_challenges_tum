f552b459e3cb1684d53b774d95eaaaec0d87d53a
Tom Wenzel <tom.wenzel@limehome.de>
2021-03-14 13:54:13 +0100

    reformatting

--- src/challenge1/challenge_1.py
+++ src/challenge1/challenge_1.py
@@ -1,3 +1,5 @@
 def main():
     print(str("Hello"))
+
+
 main()
--- src/challenge1/client1.py
+++ src/challenge1/client1.py
@@ -3,7 +3,7 @@ import socket
 from base64 import b64decode
 
 # Fill in the right target here
-HOST = 'netsec.net.in.tum.de'
+HOST = "netsec.net.in.tum.de"
 PORT = 20001
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,36 +11,36 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 sf = s.makefile("rw")  # we use a file abstraction for the sockets
 
-data = sf.readline().rstrip('\n')
+data = sf.readline().rstrip("\n")
 print("From Server: `{}'".format(data))
 
-data = sf.readline().rstrip('\n')
-print("From Server: `{}'".format(data))
-
-data = input("Your Answer: ")
-sf.write("{}\n".format(data))
-sf.flush()
-
-data = sf.readline().rstrip('\n')
+data = sf.readline().rstrip("\n")
 print("From Server: `{}'".format(data))
 
 data = input("Your Answer: ")
 sf.write("{}\n".format(data))
 sf.flush()
 
-data = sf.readline().rstrip('\n')
+data = sf.readline().rstrip("\n")
 print("From Server: `{}'".format(data))
 
-data = sf.readline().rstrip('\n')
+data = input("Your Answer: ")
+sf.write("{}\n".format(data))
+sf.flush()
+
+data = sf.readline().rstrip("\n")
+print("From Server: `{}'".format(data))
+
+data = sf.readline().rstrip("\n")
 print("From Server: received {} bytes".format(len(data)))
 
 data = b64decode(data)
 
-pdf_hdr = b'%PDF-1.5'
+pdf_hdr = b"%PDF-1.5"
 
-if len(data) >= len(pdf_hdr) and data[:len(pdf_hdr)] == pdf_hdr:
+if len(data) >= len(pdf_hdr) and data[: len(pdf_hdr)] == pdf_hdr:
     print("Looks like we got a PDF!")
-    f = open('ex.pdf', 'wb')
+    f = open("ex.pdf", "wb")
     f.write(data)
     f.flush()
 
