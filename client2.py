30841b46ac3ccfd09f943320b2ebbbd3bce216e5
Tom Wenzel <tom.wenzel@limehome.de>
2021-03-14 13:07:09 +0100

    challenge2 added

---
+++ client2.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import socket
+from base64 import b64decode
+from random import randrange
+
+
+def randnum(number: int) -> str:
+    if number < 10:
+        return f"0{str(number)}"
+    else:
+        return str(number)
+
+
+while (True):
+    HOST = 'netsec.net.in.tum.de'
+    PORT = 20002
+
+    username = "root"
+    password = f"Password{randnum(randrange(0, 100))}"
+    credentials = username + "," + password
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    s.connect((HOST, PORT))
+    sf = s.makefile("rw")  # we use a file abstraction for the sockets
+
+    sf.write("{}\n".format(credentials))
+    sf.flush()
+
+    challenge = sf.readline().rstrip('\n')
+
+    print("Solve the following equation to prove you are human: ", challenge)
+    response = eval(challenge)
+    sf.write("{}\n".format(response))
+    sf.flush()
+
+    data = sf.readline().rstrip('\n')
+    print("From Server: `{}'".format(data))
+
+    data = sf.readline().rstrip('\n')
+    print("From Server: received {} bytes".format(len(data)))
+
+    data = b64decode(data.encode())
+
+    pdf_hdr = b'%PDF-1.5'
+
+    if len(data) >= len(pdf_hdr) and data[:len(pdf_hdr)] == pdf_hdr:
+        print("Looks like we got a PDF!")
+        f = open('ex2.pdf', 'wb')
+        f.write(data)
+        f.flush()
+
+    sf.close()
+    s.close()
